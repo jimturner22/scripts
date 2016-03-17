2dd0c9f0ba901602881e3d4da2547d26f6e5a989
Jim Turner <jturner22@gmail.com>
2016-03-17 06:07:11 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scripts
+
+curl -sL https://github.com/jimturner22/scripts/basic_setup.sh | sudo -E bash -
---
+++ basic_setup.sh
@@ -0,0 +1,47 @@
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get update
+
+sudo apt-get install openssh-server
+
+
+# curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_0.12.x | sudo -E bash -
+wait
+sudo apt-get install -y nodejs
+sudo aot-get install -y node-red
+
+# sudo apt-get install -y python-software-properties
+
+sudo apt-get install -y git
+sudo apt-get install -y subversion
+sudo apt-get install -y tmux
+sudo apt-get install -y vim
+
+cd ~
+git clone https://github.com/jimturner22/.dotfiles.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd ~/.dotfiles
+sudo ./link.sh
+cd ~
+
+
+# sudo apt-get install -y make
+# sudo apt-get install -y gcc
+# sudo apt-get install -y python 
+
+# sudo apt-get install -y python-software-properties
+# sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+# sudo apt-get update
+# sudo apt-get install -y gcc-4.9
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 50
+# sudo apt-get install -y g++-4.9
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
+
+
+# sudo npm install -g node-gyp
+
+
+ 
+
+
